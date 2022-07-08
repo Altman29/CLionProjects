53dfd78ac9846672299d96c1c7d68767a028b884
Chen Siyuan <chensiyuan@zeenhealth.com>
2022-07-08 10:28:07 +0800

    feat: 07.08 10:28

---
+++ 面相对象/Lesson_2_继承.h
@@ -0,0 +1,14 @@
+//
+// Created by edy on 2022/7/7.
+//
+
+#ifndef CLIONPROJECTS_LESSON_2_继承_H
+#define CLIONPROJECTS_LESSON_2_继承_H
+
+
+class Lesson_2_继承 {
+
+};
+
+
+#endif //CLIONPROJECTS_LESSON_2_继承_H
